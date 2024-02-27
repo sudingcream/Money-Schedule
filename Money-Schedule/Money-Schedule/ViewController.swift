32e84c06402b23dc367e22aefb5deb2d809f270e
betty <betty.sujin.lee@gmail.com>
2024-02-27 16:49:15 +0900

    프로젝트 시작

---
+++ Money-Schedule/Money-Schedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money-Schedule
+//
+//  Created by betty on 2024/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
